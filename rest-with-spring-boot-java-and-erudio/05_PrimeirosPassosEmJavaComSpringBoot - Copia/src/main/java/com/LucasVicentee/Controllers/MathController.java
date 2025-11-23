f222f3f5af1a3b023cabfcec9ba3577e00a27de9
LucasVicentee <lh201612@gmail.com>
2025-11-23 20:15:17 -0300

    Inserindo a API de soma de dois números

---
+++ rest-with-spring-boot-java-and-erudio/05_PrimeirosPassosEmJavaComSpringBoot - Copia/src/main/java/com/LucasVicentee/Controllers/MathController.java
@@ -0,0 +1,34 @@
+package com.LucasVicentee.Controllers;
+
+import ch.qos.logback.classic.spi.IThrowableProxy;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.apache.tomcat.util.http.parser.HttpParser.isNumeric;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    //http://localhost:8080/math/sum/3/5
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo
+    ) throws IllegalAccessException {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new IllegalAccessException();
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String numberOne) {
+        return 1D;
+    }
+
+    private boolean isNumeric(String number) {
+        return true;
+    }
+
+    //http://localhost:8080/math/subtraction/3/5
+
+    //http://localhost:8080/math/division/3/5
+
+}
