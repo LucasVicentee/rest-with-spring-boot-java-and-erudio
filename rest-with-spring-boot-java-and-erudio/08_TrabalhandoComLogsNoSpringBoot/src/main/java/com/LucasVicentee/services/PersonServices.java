355350062dc626f2c053de041810ec4efc193abd
LucasVicentee <lh201612@gmail.com>
2025-11-27 10:24:48 -0300

    Finalização do módulo 8 do curso

--- rest-with-spring-boot-java-and-erudio/08_TrabalhandoComLogsNoSpringBoot/src/main/java/com/LucasVicentee/services/PersonServices.java
+++ rest-with-spring-boot-java-and-erudio/08_TrabalhandoComLogsNoSpringBoot/src/main/java/com/LucasVicentee/services/PersonServices.java
@@ -3,19 +3,20 @@ package com.LucasVicentee.services;
 import com.LucasVicentee.exception.ResourceNotFoundException;
 import com.LucasVicentee.model.Person;
 import com.LucasVicentee.repository.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.logging.Logger;
 
 @Service
 public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
 
-    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+    private Logger logger = LoggerFactory.getLogger(PersonServices.class.getName());
 
     @Autowired
     PersonRepository repository;
