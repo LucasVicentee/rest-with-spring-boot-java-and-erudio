ec9dbb5a4af148119ba3ab7161ca460115efbf1f
LucasVicentee <lh201612@gmail.com>
2025-12-02 21:27:42 -0300

    Finalização do módulo 11 do curso

---
+++ rest-with-spring-boot-java-and-erudio/11_AdicionandoSuporteAMigrationsComFlyway/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+(1,'São Paulo - Brasil','Ayrton','Male','Senna'),
+(2,'Presidente Prudente - SP','Lucas Henrique','Male','Vicente dos Santos'),
+(4,'São Paulo - Brasil','Sabrina Narumi (Muié Boboca)','Female','Suyama'),
+(7,'Fukushima Japão','Sabrina Linda','Female','Suyama'),
+(8,'Shangai China','Lucão','Male','Suyama');
